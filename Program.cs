83e759e28538647f9f02301fb133d1d0129ea8fa
sakethchidepudi <chidepudis@mail.usf.edu>
2019-09-10 13:44:07 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LinkedinTraining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var a = Console.ReadLine();
+            Console.WriteLine("What is your age?");
+            var b= Console.ReadLine();
+            Console.WriteLine("DOB?");
+            var c = Console.ReadLine();
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+
+        }
+    }
+}
